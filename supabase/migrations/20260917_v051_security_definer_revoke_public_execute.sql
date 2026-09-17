73bfbe956a1fd5810ba02a8732673252b5a2507c
alvesweverton7-wq <alvesweverton7@gmail.com>
2026-09-17 14:26:25 -0300

    chore(migrations): remove duplicate unversioned public execute migration

--- supabase/migrations/20260917_v051_security_definer_revoke_public_execute.sql
+++
@@ -1,6 +0,0 @@
-begin;
-revoke execute on function public.get_user_tenant_id() from public;
-revoke execute on function public.is_software_master() from public;
-grant execute on function public.get_user_tenant_id() to authenticated;
-grant execute on function public.is_software_master() to authenticated;
-commit;
