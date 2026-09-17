8ebedf4780d01e28b8ef08a6590069143f11f94a
alvesweverton7-wq <alvesweverton7@gmail.com>
2026-09-16 21:12:08 -0300

    feat: ativar contexto Auth Tenant no React

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
+import { AuthTenantProvider } from './auth/AuthTenantProvider';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><AuthTenantProvider><App /></AuthTenantProvider></React.StrictMode>,
+);
